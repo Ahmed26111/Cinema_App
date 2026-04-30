5d3c3921ce315079c95bd9b5da64db637a0f6c9c
Ahmed26111 <midoronaldo2006@gmail.com>
2026-04-30 17:58:46 +0300

    update env files

--- lib/constants/api constants/api_constants.dart
+++ lib/constants/api constants/api_constants.dart
@@ -6,14 +6,14 @@ abstract class ApiConstants{
 
   static final String baseImageUrl = dotenv.env['BaseImageUrl'] ?? "";
   static final String baseUrl = dotenv.env['BaseUrl'] ?? "";
-  static final String moviePopularEndPoint = "/movie/popular";
-  static final String movieTopRatedEndPoint = "/movie/top_rated";
-  static final String movieUpComingEndPoint = "/movie/upcoming";
-  static final String movieIdEndPoint = "/movie/"; //* need to be concatenated with id
-  static final String searchMovieEndPoint = "/search/movie";
-  static final String movieCreditsEndPoint1 = "/movie/";
-  static final String movieCreditsEndPoint2 = "/credits";
-  static final String movieSimilarEndPoint1 = "/movie/";
-  static final String movieSimilarEndPoint2 = "/similar";
+  static final String moviePopularEndPoint = dotenv.env['MoviePopularEndPoint'] ?? "";
+  static final String movieTopRatedEndPoint = dotenv.env['MovieTopRatedEndPoint'] ?? "";
+  static final String movieUpComingEndPoint = dotenv.env['MovieUpComingEndPoint'] ?? "";
+  static final String movieIdEndPoint = dotenv.env['MovieIdEndPoint'] ?? ""; //* need to be concatenated with id
+  static final String searchMovieEndPoint = dotenv.env['SearchMovieEndPoint'] ?? "";
+  static final String movieCreditsEndPoint1 = dotenv.env['MovieCreditsEndPoint1'] ?? "";
+  static final String movieCreditsEndPoint2 = dotenv.env['MovieCreditsEndPoint2'] ?? "";
+  static final String movieSimilarEndPoint1 = dotenv.env['MovieSimilarEndPoint1'] ?? "";
+  static final String movieSimilarEndPoint2 = dotenv.env['MovieSimilarEndPoint2'] ?? "";
   static final String apiKey = dotenv.env['Api_key'] ?? "";
 }
